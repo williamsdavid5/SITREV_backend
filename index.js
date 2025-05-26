4fa8837ef21180b508481d642dda2981dc60b22b
David Duarte (Williams) <davidduart04@gmail.com>
2025-05-26 14:43:42 -0300

    correcao para acesso do front

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import motoristasRoutes from './routes/motoristas.js';
 import viagensRoutes from './routes/viagens.js';
@@ -12,6 +13,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 app.use('/motoristas', motoristasRoutes);
 app.use('/viagens', viagensRoutes);
