1183ae99b6ecd594f2a5efb3ffb468ff8511701d
David Duarte (Williams) <davidduart04@gmail.com>
2025-05-28 11:00:02 -0300

    correção do cors

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 app.use('/motoristas', motoristasRoutes);
 app.use('/viagens', viagensRoutes);
